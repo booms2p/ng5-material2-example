28eddb73507c78a556514323e7174888f12ec1b6
Waramun <booms_2p@hotmail.com>
2018-03-27 21:21:27 +0700

    Update show image editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule, MatButtonModule } from '@angular/material';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { NgxImageEditorModule } from "ngx-image-editor";
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { DialogComponent } from './dialog/dialog.component';
 import { DialogDetailComponent } from './dialog/dialog-detail/dialog-detail.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -42,7 +39,8 @@ import { ImageCropperComponent } from './components/image-cropper/image-cropper.
     MatFormFieldModule,
     FlexLayoutModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    NgxImageEditorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/image-cropper/image-cropper.component.ts
+++ src/app/components/image-cropper/image-cropper.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EditorOptions } from 'ngx-image-editor';
 
 @Component({
   selector: 'app-image-cropper',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-cropper.component.css']
 })
 export class ImageCropperComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public config: EditorOptions = {
+    ImageName: 'Some image',
+    AspectRatios: ['4:3', '16:9'],
+    ImageUrl:
+      'https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg',
+    ImageType: 'image/jpeg'
+  };
+
+  public close() {
+    console.log('this is close')
   }
 
+  public getEditedFile(file: File) {
+    console.log(file)}
 }
